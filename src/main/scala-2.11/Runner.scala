344349835603cc2b11bbbb80859b93fed3f9d3c2
akanimax <akanimax@gmail.com>
2016-10-06 11:01:12 +0530

    Tester main object (for debugging)

--- src/main/scala-2.11/Runner.scala
+++ src/main/scala-2.11/Runner.scala
@@ -1,6 +1,12 @@
+import NLP.MarkovModels.{Emission, HMM, Transition}
+
 /**
   * Created by botman on 6/10/16.
   */
-object Runner {
+object Runner extends App{
 
+  val model_1 = HMM(Transition(Array("Hot", "Cold")),
+    Emission(List("1", "2", "3"), 2))
+
+    println(model_1.likelihood(List("3", "1", "3")))
 }
